53bbf286aa00685302187a15aebdaa64464f2866
selintas <sselintas@gmail.com>
2025-02-21 15:36:34 +0300

    çıktı verme

---
+++ JavaScript/Part3Cıktı/app.js
@@ -0,0 +1,17 @@
+// Document write ile çıktı verme
+// document.write("Hello World");
+// document.write("Selin World");
+// document.write("Selin World <br>");
+// document.write(5.5);
+
+
+//console.log("Hello World");
+//console.log(1234);
+//console.log({name:"Selin",age:25});
+
+//alert("Dikkat"); uyarı verir popup ile çıktı verme işlemi yapar
+
+//console.log(window);
+
+//alert("merhaba"); popup açar
+
